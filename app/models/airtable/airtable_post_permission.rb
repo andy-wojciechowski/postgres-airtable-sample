c5565a2d70557f22c0296cc763c0b230f549b44b
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-08-24 08:32:10 -0500

    Adding a sample Airrecord table

---
+++ app/models/airtable/airtable_post_permission.rb
@@ -0,0 +1,10 @@
+Airrecord.api_key = ENV['AIRTABLE_API_KEY']
+
+class AirtablePostPermission < Airrecord::Table
+  self.base_key = 'apps2Qji4xtiaz4PP'
+  self.table_name = 'PostPermissions'
+
+  def get_posts_for_user(name)
+    all(filter: "{GithubUser} = \"#{name}\"")
+  end
+end
