477c57a9c8cdf1511f96dad7e046840c4b529463
andy-wojciechowski <wojciechowskia@msoe.edu>
2019-08-24 19:38:15 -0500

    Added a require statement to require the airtable model

--- app/controllers/permission_controller.rb
+++ app/controllers/permission_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../models/airtable/airtable_post_permission'
+
 class PermissionController < ApplicationController
   def index
     @permissions = AirtablePostPermission.get_posts_for_user(params[:name]) if params[:name]
